45c33194178506ffcdbc2f3664e5d27cb314dfd1
mrp <mrkrynp@gmail.com>
2023-02-20 02:32:17 +0800

    modified login page

--- login.php
+++ login.php
@@ -17,6 +17,7 @@ if(isset($_SESSION['login_user'])){
     <link rel="stylesheet" href="style/bootstrap.css" />
     <link rel="stylesheet" href="style/login.css" />
     <title>Login to BCP Porperty Custodian</title>
+
 </head>
 
 <body>
@@ -48,16 +49,17 @@ if(isset($_SESSION['login_user'])){
                             </label>
                             <div id="login">
                                 <div class="passwordContainer">
-                                    <input type="password" class="inputField input-form form-control px-3 fs-6 fw-normal" required name="password" placeholder="Enter Password here"/>
-                                    <i class="fa-solid fa-eye-slash" id="passwordIconId"></i>
+                                    <input type="password" class="inputField input-form form-control px-3 fs-6 fw-normal" required name="password"  id="inputPassword"
+                                    placeholder="Enter Password here"/>
+                                    <!-- <i class="fa-eye" id="passwordIconId"></i> -->
                                 </div>
                             </div>
-                        </div>
+                  </div>
 
-                        <button type="submit" class="buttonTemplate sumbit-button btn rounded-2 w-100 mt-3" name="submit">
-                            Log in
-                        </button>
-                        <div class="mb-3">
+                  <button type="submit" class="buttonTemplate sumbit-button btn rounded-2 w-100 mt-3" name="submit">
+                    Log in
+                </button>
+                <div class="mt-3 text-center">
                             <?php 
                             if(isset($_REQUEST["err"]))
                                 $msg="Invalid username or Password";
@@ -68,26 +70,22 @@ if(isset($_SESSION['login_user'])){
                                 echo $msg;
                             }
                         ?></p>
-                    </div>
-                    <span class="loginLineBreak my-4"></span>
-                    <span>By using our site, you understand and agree to the BCP Property Custodian's Terms of Service and Policy Statement.</span>
-                </form>
-            </div>
-        </div>
+                    </div>            <span class="loginLineBreak my-4"></span>
+            <span>By using our site, you understand and agree to the BCP Property Custodian's Terms of Service and Policy Statement.</span>
+        </form>
     </div>
+</div>
+</div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script>
-    let showPassword = document.querySelector("#passwordIconId");
+    const showPassword = document.querySelector("#passwordIconId");
     const passwordField = document.querySelector("#inputPassword");
-
-    showPassword.addEventListener("click", function() {
-        this.classList.toggle("fa-eye");
-
+    showPassword.addEventListener("click", function(e) {
+        this.classList.toggle("fa-eye-slash");
         const type =
         passwordField.getAttribute("type") === "password" ?
-        "text" :
-        "password";
+        "text" : "password";
         passwordField.setAttribute("type", type);
     });
 </script>
