8a908ae2403e1227527991d575249671a1e70fd9
mrp <mrkrynp@gmail.com>
2023-02-19 14:03:26 +0800

    modified config file

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$DB_SERVER = "localhost";
-$DB_USER = "mark";
-$DB_PASS = "";
-$DB_NAME = "pc_db";
+$DB_SERVER = "127.0.0.1:3306";
+$DB_USER = "u476821515_mrkrynp";
+$DB_PASS = "BCPsms2022";
+$DB_NAME = "u476821515_pc_testDb";
   // Create connection
 $conn = new mysqli($DB_SERVER, $DB_USER, $DB_PASS, $DB_NAME);
   // Check connection
